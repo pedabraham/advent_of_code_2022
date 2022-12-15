60b2abece33d0b73316608de09082470d2b69c6d
Pedro A. Moreno <pedabraham@gmail.com>
2022-12-15 05:34:18 +0000

    add test

--- day_13.py
+++ day_13.py
@@ -9,20 +9,27 @@ def read_lists(input):
         top_list = final_list
         for car in line:
             if car == '[':
-                
                 new_list = list()
                 top_list.append(new_list)
                 top_list = new_list
                 list_stack.append(new_list)
-                
-            elif car == ']',
+            elif car == ']':
                 list_stack.pop()
                 if list_stack:
                     top_list = list_stack[-1]
-            elif car == ','
+            elif car == ',':
                 continue
             else:
                 number = int(car)
                 top_list.append(number)
         lists_list.append(final_list)
     return lists_list
+
+test_input = [
+    '[[1],[2,3,4]]'
+]
+
+set_list = read_lists(test_input)
+print(set_list)
+
+print(1)
