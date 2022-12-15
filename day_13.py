3047518155e143c41642b0d12d005caaec75ce84
Pedro A. Moreno <pedabraham@gmail.com>
2022-12-15 06:02:18 +0000

    add two digit numbers

--- day_13.py
+++ day_13.py
@@ -1,12 +1,13 @@
-def read_lists(input):
+def read_lists(input_lists):
     lists_list = list()
-    for line in input:
+    for line in input_lists:
         line = line.strip()
         if not line:
             continue
         list_stack = list()
         final_list = list()
         top_list = final_list
+        last_c = None
         for car in line:
             if car == '[':
                 new_list = list()
@@ -18,15 +19,41 @@ def read_lists(input):
                 if list_stack:
                     top_list = list_stack[-1]
             elif car == ',':
-                continue
+                pass
             else:
                 number = int(car)
+                if last_c not in {'[',']',',',None}:
+                    number = int(f'{last_c}{number}')
+                    top_list.pop()
                 top_list.append(number)
-        lists_list.append(final_list)
+            last_c = car
+        lists_list.append(final_list[0])
     return lists_list
 
 test_input = [
-    '[[1],[2,3,4]]'
+    '[[1],[2,3,48]]',
+    '[1,1,5,1,1]',
+    '',
+    '[[1],[2,3,4]]',
+    '[[1],4]',
+    '',
+    '[9]',
+    '[[8,7,6]]',
+    '',
+    '[[4,4],4,4]',
+    '[[4,4],4,4,4]',
+    '',
+    '[7,7,7,7]',
+    '[7,7,7]',
+    '',
+    '[]',
+    '[3]',
+    '',
+    '[[[]]]',
+    '[[]]',
+    '',
+    '[1,[2,[3,[4,[5,6,7]]]],8,9]',
+    '[1,[2,[3,[4,[5,6,0]]]],8,9]',
 ]
 
 set_list = read_lists(test_input)
